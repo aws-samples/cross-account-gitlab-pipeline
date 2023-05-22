c11d2909f877f2027e457b9371f72bfc8963d4d0
Simsek Mert <simsek97@gmail.com>
2023-05-21 20:37:39 -0400

    Add test

--- test/cross-account-gitlab.test.ts
+++ test/cross-account-gitlab.test.ts
@@ -1,17 +1,12 @@
-// import { Template } from '@aws-cdk/assertions';
-// import * as cdk from '@aws-cdk/core';
-// import * as CrossAccountGitlab from '../lib/cross-account-gitlab-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as CrossAccountGitlab from '../lib/cross-account-gitlab-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cross-account-gitlab-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CrossAccountGitlab.CrossAccountGitlabStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test('S3 Bucket Created', () => {
+  const app = new cdk.App();
+  const stack = new CrossAccountGitlab.CrossAccountGitlabStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties('AWS::S3::Bucket', {
+    enforceSSL: true
+  });
 });
