f26e2565f85be9aea28da5f51d4d67f29335ae14
muni-annachi <muni.annachi@gmail.com>
2023-08-09 11:23:19 -0400

    Update cross-account-gitlab-stack.ts

--- lib/cross-account-gitlab-stack.ts
+++ lib/cross-account-gitlab-stack.ts
@@ -1,9 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
-import * as apigw from 'aws-cdk-lib/aws-apigateway'
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
+import * as logs from 'aws-cdk-lib/aws-logs'
 import path = require('path');
 
 export class CrossAccountGitlabStack extends cdk.Stack {
@@ -11,24 +12,32 @@ export class CrossAccountGitlabStack extends cdk.Stack {
     super(scope, id, props);
 
     // Serverless App Start
-    
-    const handler = new lambda.Function(this, 'Handler', {
+
+    const lambdaRole = new iam.Role(this, 'lambdaRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+    });
+
+    const lambdaFunction = new lambda.Function(this, 'Handler', {
       code: new lambda.AssetCode('lambda'),
       handler: 'index.handler',
       runtime: lambda.Runtime.NODEJS_18_X,
+      role: lambdaRole
     });
 
-    const api = new apigw.RestApi(this, "widgets-api", {
-      restApiName: "Hello World Service",
-      description: "This is a sample api gateway service serves samples."
+    const sample_bucket = new s3.Bucket(this, 'Bucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      serverAccessLogsPrefix: 'accesslogs'
     });
 
-    const getSampleLambdaIntegration = new apigw.LambdaIntegration(handler, {
-      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
-    });
-
-    api.root.addMethod("GET", getSampleLambdaIntegration);
-    
-    // Serverless App End
+    lambdaRole.attachInlinePolicy(new iam.Policy(this, 'lambda-policy', {
+      statements: [new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [sample_bucket.bucketArn],
+      })],
+    }));
   }
 }
